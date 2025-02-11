7cc3836b1aa1212541322649e4d9666f1a31321c
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-11 18:23:11 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,50 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+	}
+	
+	public static int sum(int num, int num1) {
+		int result = num + num1;
+		return result;
 	}
 
+	public static int difference(int num, int num1) {
+		int result = num - num1;
+		return result;
+	}
+
+	public static double product(double number, double number1) {
+		double result = number * number1;
+		return result;
+	}
+
+	public static String removeFirst(String word) {
+		String result = word.substring(1);
+		return result;
+	}
+
+	public static int combinedLength(String word, String word1) {
+		int result = word.length() + word1.length();
+		return result;
+	}
+
+	public static boolean isEven(int num) {
+		boolean result = (num%2 == 0);
+		return result;
+	}
+
+	public static boolean isOdd(int num) {
+		boolean result = (num%2 != 0);
+		return result;
+	}
+
+	public static boolean isPositive(int num) {
+		boolean result = (num > 0);
+		return result;
+	}
+	public static boolean isNegative(int num) {
+		boolean result = (num < 0);
+		return result;
+	}
 }
+
